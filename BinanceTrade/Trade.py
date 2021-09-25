256ae0077d75d354d7a733b41a16137c3f9d6ae9
Puttipong Diseno <pyybott@gmail.com>
2021-09-25 22:00:16 +0700

    add

--- BinanceTrade/Trade.py
+++ BinanceTrade/Trade.py
@@ -21,22 +21,34 @@ def ReceiveSignals(signal_data_dict):
     }
     """
 
+    """
+    ex. OPEN , TPSL , BTCUSDT , 50
+    "{"ACTION":"{{strategy.order.comment}}","LEV":"50","SYMBOL":"BTCUSDT"}"
+    """
     Signal_Type = signal_data_dict["ACTION"].split(" ")[0]
     Signal_Side = signal_data_dict["ACTION"].split(" ")[1]
-    Signal_Size = 50
-    Signal_Symbol = "BTCUSDT"
+    Signal_Lev = int(signal_data_dict["LEV"])
+    Signal_Symbol = signal_data_dict["SYMBOL"]
 
     msg = ""
 
     # ให้เป็น USDT 
-    amount = Signal_Size
+    amount = 50
+
+    # CHECK WHICH SIDE TO CLOSE ?
+    CLOSE_side = "LONG"
+    if Signal_Side == "LONG":
+        CLOSE_side = "SHORT"
 
     if Signal_Type == "OPEN":
-        # ClosePositionAtMarket(symbol=Signal_Symbol, positionSide=Signal_Side)
-        PlaceOrderAtMarket(position=Signal_Side, symbol=Signal_Symbol, amount=amount, lev = 25)
+        # Close before open
+        ClosePositionAtMarket(symbol=Signal_Symbol, positionSide=CLOSE_side)
+        # open pos (NOTPSL)
+        PlaceOrderAtMarket(position=Signal_Side, symbol=Signal_Symbol, amount=amount, lev = Signal_Lev)
         msg = "ทำการ {} Position ในฝั่ง {} คู่สินค้า {} ".format(Signal_Type,Signal_Side,Signal_Symbol)
     
-    elif Signal_Type == "CLOSE":
+    elif Signal_Type == "TPSL":
+        # Close Position by the signal side ....
         ClosePositionAtMarket(symbol=Signal_Symbol, positionSide=Signal_Side)
         msg = "ทำการ {} Position ในฝั่ง {} คู่สินค้า {} ".format(Signal_Type,Signal_Side,Signal_Symbol)
     
